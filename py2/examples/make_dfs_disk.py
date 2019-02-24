35bf52a93892e25301f847ee4ba20eaf42927448
David Boddie <david@boddie.org.uk>
2019-02-24 19:17:19 +0100

    Added a missing import.

--- py2/examples/make_dfs_disk.py
+++ py2/examples/make_dfs_disk.py
@@ -17,7 +17,7 @@ You should have received a copy of the GNU General Public License
 along with this program.  If not, see <http://www.gnu.org/licenses/>.
 """
 
-import sys
+import os, sys
 import makedfs
 
 version = "1.0"
